71f09889f3ada5f521bdecfdc3954ac28e5110b2
Daniel Chi <danielhchi@gmail.com>
2019-03-19 01:19:12 -0400

    finished basic html, css, initial js load

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,30 @@
-let topics = ["Gordon Ramsey", "Guy Fieri", "Bob Ross", "Shaggy", "Jack Black", "Kizuna Ai", "Elon Musk", "Donald Trump", "Shrek"]
+//initial topics array
+let topics = ["Adventure Time", "The Simpsons", "Spongebob Squarepants", "The Boondocks", "Regular Show", "Samurai Jack", "Scooby Doo", "South Park", "Spiderman", "Family Guy"];
+//creates new button using str
+function newbtn(str) {
+    let newbtn = $("<button>").addClass("btn btn-info").text(str).attr("name", str.toLowerCase());
+    $("#btns").append(newbtn);
+};
+//initial load
+for (i = 0; i < topics.length; i++) {
+    newbtn(topics[i]);
+};
+//function to load gifs
+function gifload(str) {
+    let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cGcH9tjBLu2GddQkvgvJX4ctXpjrMMC5&q="
+                    +str+
+                    "&limit=10&offset=0";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+    });
+};
+//search button click
+$("#searchbtn").on("click", function (evt) {
+    evt.preventDefault();
+    let query = $("#query").val().trim();
+    newbtn(query);
+    gifload(query);
+});
